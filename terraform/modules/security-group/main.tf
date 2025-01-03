f655d58c92c9a950e66f664c9e2ad522738f2995
Habibur <h.rahman_96@hotmail.com>
2025-01-03 00:44:58 +0000

    refactoring

--- terraform/modules/security-group/main.tf
+++ terraform/modules/security-group/main.tf
@@ -8,6 +8,7 @@ resource "aws_security_group" "cg_sg" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Allows HTTP from anywhere
+    description = "Allow public HTTP traffic"
   }
 
   ingress {
@@ -15,21 +16,26 @@ resource "aws_security_group" "cg_sg" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Allows HTTPS from anywhere
+    description = "Allow public HTTPS traffic"
   }
 
+ # Allow public ECS container traffic (if required)
   ingress {
     from_port   = 3002           # Forwarding requests to ECS container
     to_port     = 3002
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"]  # Allow ECS container traffic from anywhere
+    description = "Allow public ECS traffic"  
   }
 
+ # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"           # Allows all outgoing traffic   
     cidr_blocks = ["0.0.0.0/0"]  
-  }
+    description = "Allow all outbound traffic"
+    }
 
   tags = {
     Name = var.sg_name
